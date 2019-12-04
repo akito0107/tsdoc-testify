ef8943ae672967e3b2a5128447296e0800caa7ca
Akito Ito <akito0107@gmail.com>
2019-12-05 02:17:01 +0900

    unnamed imports

--- src/__tests__/import.ts
+++ src/__tests__/import.ts
@@ -1,7 +1,7 @@
 import { createVirtualSource, print } from "./helper";
 import * as ts from "typescript";
 import { strict as assert } from "assert";
-import { splitImport } from "../import";
+import { mergeImports, splitImport } from "../import";
 
 test("split imports", () => {
   const source = createVirtualSource({
@@ -28,5 +28,65 @@ b();
     imports as Array<ts.Statement>
   );
 
-  assert.equal(print(importAST), `import { a, b } from "moduleA";`);
+  assert.equal(
+    print(importAST),
+    `import { a, b } from "moduleA";
+`
+  );
+});
+
+function mergeImportsRunner(src: string, expect: string) {
+  const source = createVirtualSource({
+    src,
+    fileName: "virtual.ts"
+  });
+  const { imports } = splitImport(source);
+  const results = mergeImports(imports);
+  const importSource = createVirtualSource({ src: "", fileName: "forassert" });
+  const importAST = ts.updateSourceFileNode(
+    importSource,
+    results as Array<ts.Statement>
+  );
+  assert.equal(print(importAST), expect);
+}
+
+test("merge imports", () => {
+  mergeImportsRunner(
+    `import { a, b } from "moduleA";
+import { b, c } from "moduleA";`,
+    `import { a, b, c } from "moduleA";
+`
+  );
+});
+
+test("merge imports (multi module)", () => {
+  mergeImportsRunner(
+    `import { a, b } from "moduleA";
+import { b, c } from "moduleA";,
+import { d, e } from "moduleB";
+`,
+    `import { a, b, c } from "moduleA";
+import { d, e } from "moduleB";
+`
+  );
+});
+
+test("as alias imports", () => {
+  mergeImportsRunner(
+    `import { a as alias, b as balias } from "moduleA";
+import { b as balias, c } from "moduleA";,
+`,
+    `import { a as alias, b as balias, c } from "moduleA";
+`
+  );
+});
+
+test("named imports + identifier", () => {
+  mergeImportsRunner(
+    `import React, { useCallback } from "react";
+import React, { useState, useCallback } from "react";,
+`,
+    `import React, { useCallback, useState } from "react";
+`
+  );
 });
