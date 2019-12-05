51c2b0599b1eb3d0a22dc785aa3162486c435bc7
Akito Ito <akito0107@gmail.com>
2019-12-06 01:08:32 +0900

    update licence

--- src/cli.ts
+++ src/cli.ts
@@ -1,18 +1,46 @@
-import { makeCommand, makeStringFlag } from "catacli";
+#!/usr/bin/env node
+
+import { makeCommand, makeStringFlag, mergeFlag } from "catacli";
 import * as path from "path";
+import * as glob from "glob";
 import { generate } from "./app";
 
-const flag = makeStringFlag("filepath");
+const flag = mergeFlag(
+  makeStringFlag("filepath", {
+    usage: "src file path (only single file accepted)"
+  }),
+  makeStringFlag("fileMatch", {
+    usage: "src file path (regexp)."
+  })
+);
 
 const command = makeCommand({
-  name: "tsdoc-testify",
-  description: "catacli is typescript-friendly commander tool",
+  name: "tsdoctestify",
+  description: "documentation testing generator for tsdoc",
   version: "0.0.1",
-  usage: "testdoc-testify [OPTIONS]",
+  usage: "testdoctestify [OPTIONS]",
   flag,
   handler: (_, params) => {
-    const filePath = path.resolve(process.cwd(), params.filepath.value);
-    generate({ filePath });
+    if (!params.filepath.value && !params.fileMatch.value) {
+      console.log("`--filepath` or `--fileMatch` is required.");
+    }
+
+    if (params.fileMatch.value) {
+      glob(params.fileMatch.value, (err, files) => {
+        if (err) {
+          console.error(err);
+        }
+        files.forEach(f => {
+          const filePath = path.resolve(process.cwd(), f);
+          generate({ filePath });
+        });
+      });
+    }
+
+    if (params.filepath.value) {
+      const filePath = path.resolve(process.cwd(), params.filepath.value);
+      generate({ filePath });
+    }
   }
 });
 
